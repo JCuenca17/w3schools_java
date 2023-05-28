9829992e87d542408574c4a440ce5b63454fa355
Jose Cuenca <jcuenca@unsa.edu.pe>
2023-05-28 17:07:06 -0500

    JavaMethods

---
+++ JavaMethods/src/main/java/JavaMethods.java
@@ -0,0 +1,17 @@
+
+/**
+ *
+ * @author josel
+ */
+public class JavaMethods {
+
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
