01d246bb3c1c8a9d3f199aeadc4058a0ff0f9ee7
Jose Cuenca <jcuenca@unsa.edu.pe>
2023-05-28 16:42:43 -0500

    Syntax - Output

---
+++ JavaTutorial/src/main/java/com/mycompany/javatutorial/JavaOutput.java
@@ -0,0 +1,16 @@
+package com.mycompany.javatutorial;
+
+/**
+ *
+ * @author josel
+ */
+public class JavaOutput {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+    }
+}
--- JavaTutorial/src/main/java/com/mycompany/javatutorial/JavaSyntax.java
+++ JavaTutorial/src/main/java/com/mycompany/javatutorial/JavaSyntax.java
@@ -4,7 +4,7 @@ package com.mycompany.javatutorial;
  *
  * @author josel
  */
-public class JavaHome {
+public class JavaSyntax {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
