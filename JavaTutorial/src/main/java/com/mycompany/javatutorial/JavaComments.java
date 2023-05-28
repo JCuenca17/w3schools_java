910413c3f28685b7dcbbf3ae254bd0c944671b80
Jose Cuenca <jcuenca@unsa.edu.pe>
2023-05-28 16:44:29 -0500

    Comments

---
+++ JavaTutorial/src/main/java/com/mycompany/javatutorial/JavaComments.java
@@ -0,0 +1,15 @@
+package com.mycompany.javatutorial;
+
+/**
+ *
+ * @author josel
+ */
+public class JavaComments {
+
+    public static void main(String[] args) {
+        // This is a comment
+        System.out.println("Hello World");
+        /* The code below will print the words Hello World
+            to the screen, and it is amazing */
+    }
+}
