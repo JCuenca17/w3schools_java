3d135a0541b290cc61e2ae11769dda53742bca51
Jose Cuenca <jcuenca@unsa.edu.pe>
2023-05-28 16:35:53 -0500

    HOME

---
+++ JavaTutorial/src/main/java/com/mycompany/javatutorial/JavaHome.java
@@ -0,0 +1,12 @@
+package com.mycompany.javatutorial;
+
+/**
+ *
+ * @author josel
+ */
+public class JavaHome {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
